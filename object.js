dec5343cf3df3e826df6ff3b3a240d60b28f86e9
SHumona-Akter <shumona.cse.bd@gmail.com>
2023-01-30 18:29:11 +0600

    object keys and object values

--- object.js
+++ object.js
@@ -17,9 +17,18 @@ console.log(mySelf["weight"]);
 var properties = "lastName";
 console.log(properties);
 
-// set properties
+// set object propertis properties
 mySelf.eye = "black";
 console.log(mySelf);
 
 mySelf["hear"] = "gold";
-console.log(mySelf)
+console.log(mySelf);
+
+// object a joto gulo property ase tader sob gulor nam jante and ata array akare property nam dibe
+
+var propertyNames = Object.keys(mySelf);
+
+console.log(propertyNames)
+// object a joto gulo value ase tader sob gulor man jante and ata array akare property nam dibe;
+var propertyName = Object.values(mySelf)
+console.log(propertyName)
