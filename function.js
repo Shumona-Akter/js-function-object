a21a1dd30ec39001fd58aee1065659bbaa006d81
SHumona-Akter <shumona.cse.bd@gmail.com>
2023-01-30 20:53:13 +0600

    function

---
+++ function.js
@@ -0,0 +1,9 @@
+// apu jokhon vorta korte bole
+function vortaKor(){
+    console.log("vat khulte hobe");
+    console.log("alu ber kor");
+    console.log("khosa sil");
+    console.log("aibar dol")
+}
+console.log(vortaKor())
+
